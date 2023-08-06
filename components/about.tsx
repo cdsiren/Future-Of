18270b7f2feb377222803c569115b1e04a344fff
Charlie <85839774+cdsiren@users.noreply.github.com>
2023-08-05 22:13:39 -0400

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -17,7 +17,7 @@ const About = () => {
         </div>
       </div>
       <p className="md:w-1/2">Currently building <a target="_blank" href="https://decent.xyz" className={`underline`}>decent.xyz</a>, premier payments infra for Ethereum L2's / high-throughput chains (& Eth L1 but that's a less interesting set of problems & opportunities).  I started researching crypto in 2017, publishing independent work on how Bitcoin was being used by Chinese citizens to <a target="_blank" href="https://github.com/cdsiren/capital-flight" className={`underline`}>circumvent capital controls</a>.</p>
-      <p className="md:w-1/2 py-4">In a prior life, I played professional lacrosse and won a world championship with the Cannons in 2020
+      <p className="md:w-1/2 py-4">In a prior life, I played professional lacrosse and won a world championship with the Cannons in 2020</p>
       <p className="md:w-1/2 ">Check out my <a target="_blank" href="https://twitter.com/cdurbinxyz" className={`underline`}>Twitter</a> for quick thoughts, <a target="_blank" href="https://www.linkedin.com/in/charlie-durbin-b88544131/" className={`underline`}>LinkedIn</a> for work & eduction history, and <a target="_blank" href="https://github.com/cdsiren" className={`underline`}>Github</a> for experiments.</p>
     </div>
   </>
