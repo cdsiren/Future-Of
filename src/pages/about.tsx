b47789028641359c6bc4ecbbcc3885e9707c9a69
Charlie Durbin <charlie@decent.xyz>
2024-02-11 23:11:07 -0500

    force rebuild

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import Navbar from '../components/Navbar';
+import Navbar from '../components/navbar';
 import { sanityClient } from '../lib/sanity/sanity';
 import { GetServerSideProps } from 'next';
 import About from '../components/about';
